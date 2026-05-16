9d278c3ab3b7b1a1b2b62df41437ecb8ca758cb7
alexlu07 <alexlu314@gmail.com>
2026-05-16 01:37:31 -0400

    ansi scrollback

--- templates/zellij_scrollback.sh
+++ templates/zellij_scrollback.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 exec vim \
-  +'normal G?^.<CR>$' \
-  +'exe "normal ?\\S\r"' \
-  +'nnoremap q :qa!<CR>' \
-  +'nnoremap y "+y:qa!<CR>' \
-  +'vnoremap y "+y:qa!<CR>' \
-  "$@"
+  -c 'set nonumber norelativenumber termguicolors' \
+  -c 'nnoremap q :qa!<CR>' \
+  -c 'nnoremap y "+y:qa!<CR>' \
+  -c 'vnoremap y "+y:qa!<CR>' \
+  -c "call term_start(['cat', '$1'], {'close_cb': {_ -> feedkeys(\"G\$?^.\<CR>\$\",'n')}, 'curwin': 1})"
+
