ba4156d0bc2b9441de5a2a4eef4d4e6183f44acd
alexlu07 <alexlu314@gmail.com>
2026-05-17 12:54:04 -0400

    delay scrollback send keys

--- templates/zellij_scrollback.sh
+++ templates/zellij_scrollback.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+keys="G\$?^.\<CR>\$"
+close_cb="timer_start(50, {_ -> feedkeys(\"$keys\", 'n')})"
+term_opts="{'close_cb': {_ -> $close_cb}, 'curwin': 1}"
+
 exec vim \
   -c 'set nonumber norelativenumber' \
   -c 'nnoremap q :qa!<CR>' \
   -c 'nnoremap y "+y:qa!<CR>' \
   -c 'vnoremap y "+y:qa!<CR>' \
-  -c "call term_start(['cat', '$1'], {'close_cb': {_ -> feedkeys(\"G\$?^.\<CR>\$\",'n')}, 'curwin': 1})"
-
+  -c "call term_start(['cat', '$1'], $term_opts)"
