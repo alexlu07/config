68b2ea86a77bd457e0026f431d828e3cd2ea57f0
alexlu07 <alexlu314@gmail.com>
2026-05-28 10:23:59 -0500

    theme

--- templates/zellij/zellij_scrollback.sh
+++ templates/zellij/zellij_scrollback.sh
@@ -7,6 +7,7 @@ term_opts="{'close_cb': {_ -> $close_cb}, 'curwin': 1}"
 exec vim \
   -c 'set nonumber norelativenumber' \
   -c 'nnoremap q :qa!<CR>' \
+  -c 'vnoremap q :<C-u>qa!<CR>' \
   -c 'nnoremap y y:OSCYankRegister<CR>:qa!<CR>' \
   -c 'vnoremap y y:OSCYankRegister<CR>:qa!<CR>' \
   -c "call term_start(['cat', '$1'], $term_opts)"
